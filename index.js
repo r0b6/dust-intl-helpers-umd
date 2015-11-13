a515d899d51500af0ec0c8ca140433fb39812ad1
Robb Samuell <robert.samuell@hyatt.com>
2015-11-12 21:01:48 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+(function(root, factory) {
+  if (typeof define === 'function' && define.amd && define.amd.dust === true) {
+    define(['dust.core'], factory);
+  } else if (typeof exports === 'object') {
+    module.exports = factory(require('dustjs-linkedin'));
+  } else {
+    factory(root.dust);
+  }
+}(this, function(dust) {
+	var dustIntl = require('dust-intl');
+
+	dustIntl.registerWith(dust);
+
+	return dust;
+}));
