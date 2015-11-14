7c468a19a4a27384ceb453f2f46842123eb4bbe4
Robb Samuell <robert.samuell@hyatt.com>
2015-11-14 13:14:35 -0600

    Move polyfill to module export scenario

--- index.js
+++ index.js
@@ -2,23 +2,22 @@
   if (typeof define === 'function' && define.amd && define.amd.dust === true) {
     define(['dust.core'], factory);
   } else if (typeof exports === 'object') {
+    // These are our polyfills.
+
+    if (global.Intl) {
+        // `Intl` exists, load the polyfill and replace the constructors with need with the polyfill's.
+        require('intl');
+        Intl.NumberFormat = global.IntlPolyfill.NumberFormat;
+        Intl.DateTimeFormat = global.IntlPolyfill.DateTimeFormat;
+    } else {
+        // No `Intl`, so use and load the polyfill.
+        global.Intl = require('intl');
+    }
     module.exports = factory(require('dustjs-linkedin'));
   } else {
     factory(root.dust);
   }
 }(this, function(dust) {
-  // These are our polyfills.
-
-  if (global.Intl) {
-      // `Intl` exists, load the polyfill and replace the constructors with need with the polyfill's.
-      require('intl');
-      Intl.NumberFormat = global.IntlPolyfill.NumberFormat;
-      Intl.DateTimeFormat = global.IntlPolyfill.DateTimeFormat;
-  } else {
-      // No `Intl`, so use and load the polyfill.
-      global.Intl = require('intl');
-  }
-
   var dustIntl = require('dust-intl');
   dustIntl.registerWith(dust);
   return dust;
